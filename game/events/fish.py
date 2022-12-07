25def15f57e984c77a5b6cf9876520451cc6f85f
Jeremy <jjune002@plattsburgh.edu>
2022-12-07 16:41:13 -0500

    almost done fishing, thought of other things to add.

--- game/events/fish.py
+++ game/events/fish.py
@@ -5,6 +5,8 @@ from game.context import Context
 from game.display import announce
 import game.config as config
 import game.ship as ship
+import game.world as world
+import game.events.drowned_pirates as drowned_pirates
 
 class Fish (Context, event.Event):
 
@@ -13,7 +15,7 @@ class Fish (Context, event.Event):
         self.name = "school of fish"
         self.fish = 1
         self.verbs['ignore'] = self
-        self.verbs['catch'] = self
+        self.verbs['fish'] = self
         self.verbs['help'] = self
         self.result = {}
         self.go = False
@@ -21,10 +23,10 @@ class Fish (Context, event.Event):
     def process_verb (self, verb, cmd_list, nouns):
         if (verb == 'ignore'):
             self.go = True
-        elif (verb == 'catch'):
+        elif (verb == 'fish'):
             self.go = True
             r = random.randint(1,10)
-            if (r=10):
+            if (r==10):
                 self.result["message"] = 'You caught a huge swordfish. This will feed your crew for weeks.'
                 self.food = self.food + 50
             elif (9>=r>7):
@@ -34,10 +36,24 @@ class Fish (Context, event.Event):
                 self.result["message"] = 'You caught a tiny sunfish. Makes a good snack.'
                 self.food = self.food + 10
             else:
-                
+                drowned_pirates.DrownedPirates().process
 
         elif (verb == "help"):
             print('The crew notices a school of fish passing through, you can try and catch or ignore')
             self.go = False
         else:
             print('Would you like to ignore or start fishing')
+            self.go = False
+            
+    def process (self, world):
+
+            self.go = False
+            self.result = {}
+            self.result["newevents"] = [ self ]
+            self.result["message"] = "default message"
+
+            while (self.go == False):
+                print (str (self.fish) + " school of fish has appeared, what do you want to do?")
+                Player.get_interaction ([self])
+
+            return self.result
